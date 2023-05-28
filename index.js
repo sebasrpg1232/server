8497a4193c46ed0c04c83b5a9526ff305ea28b85
LAPTOP <sebasrpg1232@gmail.com>
2023-05-28 13:24:15 -0500

    primerCommit

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require('express')
+const app = express()
+const mysql = require('mysql')
+const cors = require('cors')
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password:'',
+    database: 'taller'
+});
+
+
+app.post("/Form", (req, res) => {
+    const referencia = req.body.referencia
+    const color = req.body.color
+    const talla = req.body.talla
+    const precio = req.body.precio
+    db.query('INSERT INTO producto (referencia, color, talla, precio) VALUES (?,?,?,?)',[referencia, color, talla, precio],
+    (err, result)=>{
+        if(err){
+            console.log(err)
+        }else{
+            res.send("Product Created")
+        }
+    }
+    
+    );
+});
+
+app.get("/products", ( req, res) => {
+  db.query("select * from producto", function(err, result, fields){
+    if(err) throw err;
+    console.log("result" +result);
+    res.send(result);
+  })
+
+    
+        
+
+//   db.query("SELECT * FROM producto", function ( result, fields) {
+//     ( result)=>{
+       
+//             console.log(result)
+//             res.json(result)
+        
+//     }
+//   });
+});
+
+
+  app.get("/image", ( req, res, next) => {
+    let params = req.query.imagen
+  db.query("select * from images where referencia = ?",[params], function(err, result, fields){
+    if(err) throw err;
+    console.log("result" +result);
+    res.send(result);
+  })
+})
+
+app.listen(3002,()=> {
+    console.log("Corriendo en el puerto 3002")
+});
